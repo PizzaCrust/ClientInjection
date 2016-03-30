377a7dcde865b93ac7f3689a9f25c5e731a579e6
PizzaCrust <torchprojectserver@gmail.com>
2016-03-30 12:04:10 +0700

    minecraft mixin for 1.9

---
+++ src/main/java/net/pizzacrust/cinject/MinecraftMixin.java
@@ -0,0 +1,23 @@
+package net.pizzacrust.cinject;
+
+import net.pizzacrust.mixin.Inject;
+import net.pizzacrust.mixin.Mixin;
+
+/**
+ * A minecraft client injection.
+ *
+ * @author PizzaCrust
+ */
+@Mixin(InjectionAgent.MINECRAFT_CLASS_MAPPING)
+public class MinecraftMixin {
+    /**
+     * Declares the startGame method here, as a method injection.
+     */
+    @Inject(Inject.Execution.AFTER)
+    /**
+     * The startGame method with a obfuscated name.
+     */
+    public static void an() {
+        System.out.println("ClientInjection > Injection has injected to the client successfully!");
+    }
+}
